0be1fbe9b93173a9877547a7c12de05afb275ed5
Thawfik <ayanathawfik7@gmail.com>
2025-08-08 06:25:31 +0000

    Changes to be committed: 	modified:   PPE301/PPE301/settings.py 	modified:   requirements.txt

--- PPE301/PPE301/settings.py
+++ PPE301/PPE301/settings.py
@@ -80,7 +80,9 @@ WSGI_APPLICATION = 'PPE301.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 
